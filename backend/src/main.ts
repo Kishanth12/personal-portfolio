ae82a9418995bb46a11db8877293bf7c4f567863
kishanth12 <kishanth1209@gmail.com>
2026-01-08 21:36:20 +0530

    feat(core): add global validation, response interceptor, CORS, and API prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './common/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.enableCors({
+    origin: 'http://localhost:3000', // Next.js
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove unknown fields
+      forbidNonWhitelisted: true, // throw error if extra fields sent
+      transform: true, // auto type conversion
+    }),
+  );
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
